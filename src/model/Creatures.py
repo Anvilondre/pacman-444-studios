2b02c3ef73b856ab0f1a8884657d2170b50f73c0
Roman Burakov <buracov.r@gmail.com>
2020-06-05 10:36:12 +0300

    Initial commit

---
+++ src/model/Creatures.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+
+class Creature(ABC):
+
+    @property
+    @abstractmethod
+    def x(self):
+        pass
+
+
+class PacMan(Creature):
+    def __init__(self):
+        pass
+
+    @property
+    def x(self):
+        return 1
+
+
+pac = PacMan()

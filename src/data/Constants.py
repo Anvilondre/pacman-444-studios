2f525c9da07531a0aaf23a6cee2fcd073fbcd920
Volodymyr Barannik <vovabarannik@gmail.com>
2020-06-06 14:29:52 +0300

    Renderer constants added

--- src/data/Constants.py
+++ src/data/Constants.py
@@ -1,8 +1,24 @@
 # ************************************ GAME ************************************
 SECTOR_SIZE = 36
+# ************************************ RENDERER.PY ************************************
+IS_FULLSCREEN = True
+WINDOWED_SCREEN_WIDTH = 960
+WINDOWED_SCREEN_HEIGHT = 540
+TOP_BAR_HEIGHT_RATIO = 1 / 22
+TOP_BAR_WIDTH_RATIO = 1000 / 1920
+BOTTOM_BAR_HEIGHT_RATIO = 1 / 22
+BOTTOM_BAR_WIDTH_RATIO = 1000 / 1920
+LIVES_BAR_WIDTH = 350 / 1000
+ABILITIES_BAR_WIDTH = 450 / 1000
+FRUITS_BAR_WIDTH = 200 / 1000
+
+GAMESCREEN_BOUNDBOX_SURF_WIDTH_RATIO = 1920 / 1920
+GAMESCREEN_BOUNDBOX_SURF_HEIGHT_RATIO = 980 / 1080
+
+GAMESCREEN_CELL_SIZE_RATIO = 36 / 1920
+SCREEN_BACKGROUND_IMAGE = "screen_background_image path"
 
 # ************************************ CREATURES.PY ************************************
-
 pacman_hitbox_path = "../../res/hitbox/pacman.png"
 pacman_animations_paths = {"move_left":  ["../../res/animations/pacman/move_left/0.png",
                                           "../../res/animations/pacman/move_left/1.png",
@@ -24,7 +40,7 @@ pacman_animations_paths = {"move_left":  ["../../res/animations/pacman/move_left
                                           "../../res/animations/pacman/dead/1.png",
                                           "../../res/animations/pacman/dead/2.png",
                                           "../../res/animations/pacman/dead/3.png"]}
-
+pacman_cooldown = 30
 pacman_mana = 0
 pacman_score = 0
 pacman_lives = 3
